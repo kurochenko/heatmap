f876508b292bfd74c0e374a7f0c688f06d09749f
Andrej Kuročenko <kurochenko@gmail.com>
2015-10-17 09:41:33 +0200

    Public transport calendars importer lambda.

---
+++ lambdas/imports/timetables/src/main/java/net/kurochenko/lambda/imports/Calendar.java
@@ -0,0 +1,34 @@
+package net.kurochenko.lambda.imports;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * AWS Lambda for unzipping file from one bucket to other
+ *
+ * @author kurochenko
+ */
+public class Calendar extends AbstractImporter {
+
+    @Override
+    protected PreparedStatement populateStatement(PreparedStatement ps, String[] strings) throws SQLException {
+        ps.setInt(1, Integer.parseInt(strings[0]));
+        ps.setInt(2, Integer.parseInt(strings[1]));
+        ps.setInt(3, Integer.parseInt(strings[2]));
+        ps.setInt(4, Integer.parseInt(strings[3]));
+        ps.setInt(5, Integer.parseInt(strings[4]));
+        ps.setInt(6, Integer.parseInt(strings[5]));
+        ps.setInt(7, Integer.parseInt(strings[6]));
+        ps.setInt(8, Integer.parseInt(strings[7]));
+        ps.setInt(9, Integer.parseInt(strings[8]));
+        ps.setInt(10, Integer.parseInt(strings[9]));
+
+
+        return ps;
+    }
+
+    @Override
+    protected String getQuery() {
+        return "insert into calendar values (?,?,?,?,?,?,?,?,?,?)";
+    }
+}
