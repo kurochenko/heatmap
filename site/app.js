cc2c111c0ea6f8d72e95237c7cc73bd5dc0cd475
Dávid Ocetník <ocetnik.david@gmail.com>
2015-10-17 05:37:05 +0200

    [WIP] Get heat point

--- site/app.js
+++ site/app.js
@@ -2,7 +2,7 @@ var map, heatmap;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13,
+    zoom: 6,
     center: {lat: 50.075538, lng: 14.437800},
     mapTypeId: google.maps.MapTypeId.SATELLITE
   });
@@ -45,22 +45,66 @@ function changeOpacity() {
   heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
 }
 
-// Heatmap data: 500 Points
 function getPoints() {
 
   var points = [];
 
-  var lat = 50.075538; 
-  var lng = 14.437800;
-  for (var i = 0; i < 100; i++) {
-    points.push(new google.maps.LatLng(lat, lng));
-  }
-
-  lat = 50.076091;
-  lng = 14.43037;
-  for (var i = 0; i < 50; i++) {
-    points.push(new google.maps.LatLng(lat, lng));
-  }
+  points = points.concat(getHeatPoint(50.0833, 14.4167, 496, 1247000)); // Prague
+  points = points.concat(getHeatPoint(49.2000, 16.6167, 230.2, 378327)); // Brno
+  points = points.concat(getHeatPoint(50.0386, 15.7792, 77.71, 89552)); // Pardubice
 
   return points;
+}
+
+function getHeatPoint(lat, lng, area, population) {
+    var points = [];
+    var radius = Math.sqrt(area) / 2;
+
+    var latRightBorder = lat + kmToLat(radius);
+    var lngRightBorder = lng + kmToLng(radius, latRightBorder);
+
+    var latLeftBorder = lat - kmToLat(radius);
+    var lngLeftBorder = lng - kmToLng(radius, latLeftBorder);
+
+    for (var i = 0; i < population / 1000; i++) {
+        points.push(new google.maps.LatLng(lat, lng));
+        points.push(new google.maps.LatLng(latRightBorder, lngRightBorder));
+        points.push(new google.maps.LatLng(latLeftBorder, lngLeftBorder));
+
+        var latToAdd;
+        var lngToAdd;
+
+        var R1 = Math.floor((Math.random() * radius) - radius);
+        if (R1 > 0) {
+            latToAdd = lat + kmToLat(R1);
+        }
+        else {
+            latToAdd = lat - kmToLat(Math.abs(R1));
+        }
+
+        var R2 = Math.floor((Math.random() * radius) - radius);
+        if (R2 > 0) {
+            lngToAdd = lng + kmToLng(R2);
+        }
+        else {
+            lngToAdd = lng - kmToLng(Math.abs(R2), latToAdd);
+        }
+
+        points.push(
+            new google.maps.LatLng(
+                latToAdd,
+                lngToAdd
+            )
+        );
+    }
+
+    return points;
+}
+
+function kmToLat(km) {
+    return km / 110.574;
+}
+
+function kmToLng(km, lat) {
+    return km / (111.320 * Math.cos(lat));
 }
